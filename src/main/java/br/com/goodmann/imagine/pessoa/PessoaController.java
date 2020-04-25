1c1900e11d1669cc4bcaad7b7d828f111513f2b5
Alexandre Goodmann <alexandregoodmann@gmail.com>
2020-04-25 10:57:14 -0400

    padrões para controller

--- src/main/java/br/com/goodmann/imagine/pessoa/PessoaController.java
+++ src/main/java/br/com/goodmann/imagine/pessoa/PessoaController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/pessoa")
+@RequestMapping(value = "v1/pessoas")
 public class PessoaController {
 
 	private static final Logger logger = LoggerFactory.getLogger(PessoaController.class);
@@ -28,29 +29,6 @@ public class PessoaController {
 
 	@Autowired
 	private PessoaRepository repo;
-
-	@PostMapping
-	public ResponseEntity<String> create(@RequestBody Pessoa pessoa) {
-		return new ResponseEntity<String>(service.create(pessoa), HttpStatus.CREATED);
-	}
-
-	@PutMapping
-	public ResponseEntity<String> update(@RequestBody Pessoa pessoa) {
-		logger.trace(pessoa.toString());
-		return new ResponseEntity<String>(service.update(pessoa), HttpStatus.OK);
-	}
-
-	@PatchMapping
-	public ResponseEntity<String> updatePartial(@RequestBody Pessoa pessoa) {
-		logger.trace(pessoa.toString());
-		return new ResponseEntity<String>(service.update(pessoa), HttpStatus.OK);
-	}
-
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable(required = true, name = "id") String id) {
-		this.repo.deleteById(id);
-		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Pessoa> find(@PathVariable(required = true, name = "id") String id) {
@@ -68,6 +46,32 @@ public class PessoaController {
 			return new ResponseEntity<Pessoa>(obj, HttpStatus.OK);
 		}
 		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+
+	@PostMapping
+	public ResponseEntity<String> create(@RequestBody Pessoa pessoa) {
+		logger.trace("[CREATE] - " + pessoa.toString());
+		return new ResponseEntity<String>(service.create(pessoa), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<String> update(@PathVariable(required = true, name = "id") String id,
+			@RequestBody Pessoa pessoa) {
+		logger.trace("[PUT] - " + pessoa.toString());
+		return new ResponseEntity<String>(service.update(pessoa), HttpStatus.OK);
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<String> updatePartial(@PathVariable(required = true, name = "id") String id,
+			@RequestBody Pessoa pessoa) throws NotFoundException {
+		logger.trace("[PATCH] - " + pessoa.toString());
+		return new ResponseEntity<String>(service.updatePartial(pessoa), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable(required = true, name = "id") String id) {
+		this.repo.deleteById(id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 
 }
--- src/main/java/br/com/goodmann/imagine/pessoa/PessoaService.java
+++ src/main/java/br/com/goodmann/imagine/pessoa/PessoaService.java
@@ -1,16 +1,13 @@
 package br.com.goodmann.imagine.pessoa;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.goodmann.imagine.core.ValidaCPF;
 
 @Service
 public class PessoaService {
-
-	private static final Logger logger = LoggerFactory.getLogger(PessoaController.class);
 
 	@Autowired
 	private PessoaRepository repo;
@@ -42,6 +39,37 @@ public class PessoaService {
 		}
 
 		return this.repo.save(pessoa).getId();
+	}
+
+	public String updatePartial(Pessoa pessoa) throws NotFoundException {
+
+		if ("".equals(pessoa.getId()) || pessoa.getId() == null) {
+			throw new RuntimeException("Você precisa informar o ID pessoa");
+		}
+
+		Pessoa target = this.repo.findById(pessoa.getId()).orElseThrow(NotFoundException::new);
+
+		if (pessoa.getCpf() != null) {
+			if (!ValidaCPF.isCPF(pessoa.getCpf())) {
+				throw new RuntimeException("CPF inválido");
+			}
+			target.setCpf(pessoa.getCpf());
+		}
+
+		if (pessoa.getEmail() != null)
+			target.setEmail(pessoa.getEmail());
+
+		if (pessoa.getNome() != null)
+			target.setNome(pessoa.getNome());
+
+		if (pessoa.getObs() != null)
+			target.setObs(pessoa.getObs());
+
+		if (pessoa.getTelefone() != null)
+			target.setTelefone(pessoa.getTelefone());
+
+		return this.repo.save(target).getId();
+
 	}
 
 }
